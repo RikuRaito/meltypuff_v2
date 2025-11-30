3f4d8e0a5eea01a5411381ff4e7f74121f4fff8f
RikuRaito <sairikuneont@gmail.com>
2025-11-30 23:01:22 +1100

    フッターの作成途中

--- app/components/layouts/footer/ShopFooter.jsx
+++ app/components/layouts/footer/ShopFooter.jsx
@@ -1,8 +1,11 @@
 "use client";
 
 import Footer from "./Footer";
+import Footer_cart from "./Footer_cart";
 import { usePathname } from "next/navigation";
 
-export default function Footer() {
+export default function ShopFooter() {
   const pathname = usePathname();
+  if (pathname.startsWith("/shop/cart")) return <Footer_cart />;
+  else return <Footer />;
 }
