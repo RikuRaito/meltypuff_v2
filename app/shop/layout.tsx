147bbaca74c9e66d4f54f70ad2c2dfa459494144
RikuRaito <sairikuneont@gmail.com>
2026-04-28 20:58:55 +0900

    Headerを統合

--- app/shop/layout.tsx
+++ app/shop/layout.tsx
@@ -3,11 +3,11 @@ import ShopFooter from "@/src/components/common/ShopFooter";
 import ShopHeader from "@/src/components/common/ShopHeader";
 
 export default function ShopLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="flex min-h-screen flex-col bg-white">
-      <ShopHeader />
-      <main className="flex-1 bg-white">{children}</main>
-      <ShopFooter />
-    </div>
-  );
+    return (
+        <div className="flex min-h-screen flex-col bg-white">
+            <ShopHeader />
+            <main className="flex-1 bg-white">{children}</main>
+            <ShopFooter />
+        </div>
+    );
 }
