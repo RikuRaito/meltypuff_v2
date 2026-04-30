06bd120a78548f89b6e9ce8596153dcb09aea893
RikuRaito <sairikuneont@gmail.com>
2026-05-01 01:08:28 +0900

    商品ページの実装

--- src/hooks/useProductCard.tsx
+++ src/hooks/useProductCard.tsx
@@ -1,3 +1,20 @@
+import { CartItem } from "../types/CartItem";
+
 export const useProductCard = () => {
-  const handleQtyChange = () => {};
+  const handleAddToCart = (productId: number, qty: number) => {
+    const cartRaw = localStorage.getItem("meltypuff_cart");
+    const cartArray: CartItem[] = cartRaw ? JSON.parse(cartRaw) : [];
+    const existingItem = cartArray.find(
+      (item) => item.id === productId && item.type === "non",
+    );
+
+    if (existingItem) {
+      existingItem.qty += qty;
+    } else {
+      cartArray.push({ id: productId, qty: qty, type: "non" });
+    }
+    localStorage.setItem("meltypuff_cart", JSON.stringify(cartArray));
+    window.dispatchEvent(new Event("cartUpdated"));
+  };
+  return { handleAddToCart };
 };
