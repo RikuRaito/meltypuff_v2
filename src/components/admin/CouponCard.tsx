87de9b63da03b871b08814d234b46599bce0cf75
RikuRaito <sairikuneont@gmail.com>
2026-05-08 09:40:34 +0900

    クーポンの有効/無効を切り替えるapiの実装

--- src/components/admin/CouponCard.tsx
+++ src/components/admin/CouponCard.tsx
@@ -1,6 +1,17 @@
+"use client";
 import { Coupon, CouponType } from "@prisma/client";
+import { useState } from "react";
 
 export const CouponCard = ({ coupon }: { coupon: Coupon }) => {
+  const [isChangeActiveOpen, setIsChangeActiveOpen] = useState(false);
+
+  if (isChangeActiveOpen) {
+    return (
+      <>
+        <div className="fixed" />
+      </>
+    );
+  }
   return (
     <div className="rounded-lg bg-white p-6 shadow">
       <div className="flex items-center justify-between">
@@ -13,15 +24,23 @@ export const CouponCard = ({ coupon }: { coupon: Coupon }) => {
               : `¥${coupon.discountRate}OFF`}
           </p>
         </div>
-        <span
-          className={`px-3 py-1 rounded-full text-sm font-semibold ${
-            coupon.isActive
-              ? "bg-green-100 text-green-800"
-              : "bg-gray-100 text-gray-500"
-          }`}
-        >
-          {coupon.isActive ? "有効" : "無効"}
-        </span>
+        <div className="flex flex-col justify-between gap-4  items-center ">
+          <span
+            className={`px-3 py-1 rounded-full w-fit text-sm font-semibold ${
+              coupon.isActive
+                ? "bg-green-100 text-green-800"
+                : "bg-gray-100 text-gray-500"
+            }`}
+          >
+            {coupon.isActive ? "有効" : "無効"}
+          </span>
+          <button
+            className="text-black border border-gray-300 text-sm rounded-full font-semibold px-3 py-1"
+            onClick={() => setIsChangeActiveOpen(true)}
+          >
+            {coupon.isActive ? "無効にする" : "有効にする"}
+          </button>
+        </div>
       </div>
     </div>
   );
