f03904919336bbfa67db35a59e53da7d6606a636
RikuRaito <sairikuneont@gmail.com>
2026-05-10 10:52:04 +0900

    apple payボタン実装

--- src/types/square.d.ts
+++ src/types/square.d.ts
@@ -8,8 +8,33 @@ interface SquareCard {
   destroy: () => Promise<void>;
 }
 
+interface SquarePaymentRequest {
+  countryCode: string;
+  currencyCode: string;
+  total: { amount: string; label: string };
+}
+
+interface SquareApplePayTokenizeEvent {
+  detail: { token: string };
+}
+
+interface SquareApplePay {
+  attach: (selector: string) => Promise<void>;
+  tokenize: () => Promise<{
+    status: string;
+    token?: string;
+    errors?: { message: string }[];
+  }>;
+  addEventListener: (
+    event: "ontokenize",
+    handler: (event: SquareApplePayTokenizeEvent) => void,
+  ) => void;
+}
+
 interface SquarePayments {
   card: () => Promise<SquareCard>;
+  paymentRequest: (request: SquarePaymentRequest) => SquarePaymentRequest;
+  applePay: (paymentRequest: SquarePaymentRequest) => Promise<SquareApplePay>;
 }
 
 interface SquareSDK {
