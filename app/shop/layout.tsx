2d84a969ae032846b5898499ef663dd4e27b6154
RikuRaito <sairikuneont@gmail.com>
2026-05-17 18:39:22 +0900

    Square formのscriptを追加

--- app/shop/layout.tsx
+++ app/shop/layout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import Script from "next/script";
 import ShopFooter from "@/src/components/common/ShopFooter";
 import ShopHeader from "@/src/components/common/ShopHeader";
 import AgeConfirmModal from "@/src/components/common/AgeConfirmModal";
@@ -6,6 +7,7 @@ import AgeConfirmModal from "@/src/components/common/AgeConfirmModal";
 export default function ShopLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col bg-white">
+      <Script src={process.env.NEXT_PUBLIC_SQUARE_JS_URL!} strategy="beforeInteractive" />
       <AgeConfirmModal />
       <ShopHeader />
       <main className="flex-1 bg-white">{children}</main>
